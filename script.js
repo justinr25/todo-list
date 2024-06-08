fcce5bd29a42a591e639902dfee11d2be3337f36
Justin <justin06.rosales@gmail.com>
2024-06-08 00:46:53 -0400

    saved todos to local storage and loaded them each page reload added comments

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+// get DOM elements
 const form = document.querySelector("form")
 const input = document.querySelector("[name='todo']")
 const todoList = document.getElementById("todoList")
 
+// load todos from local storage
+const existingTodos = JSON.parse(localStorage.getItem('todos'))
+const todoData = existingTodos || []
+
+// define function that adds todo to todo list
 const addTodo = (todo) => {
+	// add todo list element
 	const li = document.createElement("li")
 	li.innerHTML = todo
 	todoList.appendChild(li)
+
+	// store todo in array and save to local storage
+	todoData.push(todo)
+	localStorage.setItem('todos', JSON.stringify(todoData))
 }
 
-form.onsubmit = () => {
+// add todo to todo list
+form.onsubmit = (event) => {
 	event.preventDefault()
 	addTodo(input.value)
 	form.reset()
