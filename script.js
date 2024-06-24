ba0f401838fdf6a577461059f1e87a5d903ffbdc
Justin <justin06.rosales@gmail.com>
2024-06-24 09:44:56 +0800

    changed function declarations to arrow functions

--- script.js
+++ script.js
@@ -7,64 +7,30 @@ const clearButton = document.querySelector('[data-clear-button]')
 
 // define globals
 const LOCAL_STORAGE_LIST_KEY = 'task.list'
+const priorityFlags = {
+    'No Priority': 'assets/no-priority-flag.png',
+    'Low Priority': 'assets/low-priority-flag.png',
+    'Medium Priority': 'assets/medium-priority-flag.png',
+    'High Priority': 'assets/high-priority-flag.png'
+}
+
+// define utility functions
+const clearElement = (element) => {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
 
 // load and store todos from local storage
 const todoData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
 
-// handle new task event
-newTodoForm.addEventListener('submit', e => {
-    e.preventDefault()
-    newTodoName = newTodoInput.value
-    if (newTodoName == null || newTodoName == '') return
-    addTodo(newTodoName)
-    newTodoInput.value = null
-    saveAndRender()
-})
-
-// handle checking off task event
-todosContainer.addEventListener('click', e => {
-    if (e.target.tagName.toLowerCase() === 'input') {
-        const selectedTask = todoData.find(todo => todo.id === e.target.id)
-        selectedTask.complete = e.target.checked
-        saveAndRender()
-    }
-})
-
-// handle clear tasks event
-clearButton.addEventListener('click', e => {
-    clearElement(todosContainer)
-    todoData.length = 0
-    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify([]))
-    saveAndRender()
-})
-
-// define function to save and render
-function saveAndRender() {
-    save()
-    render()
-}
-
 // define function to save
-function save() {
+const save = () => {
     localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(todoData))
 }
 
-// define function to render
-function render() {
-    clearElement(todosContainer)
-    todoData.forEach(todo => {
-        renderTodo(todo)
-    })
-    renderRemainingTasksCount()
-}
-
-// define function that creates a todo
-function createTodo(name) {
-    return {id: Date.now().toString(), name: name, complete: false}
-}
-
 // define function that renders a todo
-function renderTodo(todo) {
+const renderTodo = (todo) => {
     // <div class="todo">
     //     <input type="checkbox" id="task-1">
     //     <label for="task-1">
@@ -97,25 +63,68 @@ function renderTodo(todo) {
     todosContainer.appendChild(todoDiv)
 }
 
-// define function that adds todos to the todo list
-function addTodo(name) {
-    newTodo = createTodo(name)
-    todoData.push(newTodo)
-}
-
 // define function that renders the count of remaining tasks
-function renderRemainingTasksCount() {
+const renderRemainingTasksCount = () => {
     const incompleteTaskCount = todoData.filter(todo => !todo.complete).length
     const taskString = incompleteTaskCount === 1 ? 'Task' : 'Tasks'
     tasksRemainingElement.innerHTML = `${incompleteTaskCount} ${taskString} Remaining`
 }
 
-// define function that clears an element
-function clearElement(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
+// define function to render
+const render = () => {
+    clearElement(todosContainer)
+    todoData.forEach(todo => {
+        renderTodo(todo)
+    })
+    renderRemainingTasksCount()
 }
+
+// define function to save and render
+const saveAndRender = () => {
+    save()
+    render()
+}
+
+// define function that creates a todo
+const createTodo = (name) => {
+    return {id: Date.now().toString(), name: name, complete: false}
+}
+// const createTodo = (name, priority) => {
+//     return {id: Date.now().toString(), name: name, priority: priority, complete: false}
+// }
+
+// define function that adds todos to the todo list array
+const addTodo = (name) => {
+    newTodo = createTodo(name)
+    todoData.push(newTodo)
+}
+
+// handle new task event
+newTodoForm.addEventListener('submit', e => {
+    e.preventDefault()
+    newTodoName = newTodoInput.value
+    if (newTodoName == null || newTodoName == '') return
+    addTodo(newTodoName)
+    newTodoInput.value = null
+    saveAndRender()
+})
+
+// handle checking off task event
+todosContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'input') {
+        const selectedTask = todoData.find(todo => todo.id === e.target.id)
+        selectedTask.complete = e.target.checked
+        saveAndRender()
+    }
+})
+
+// handle clear tasks event
+clearButton.addEventListener('click', e => {
+    clearElement(todosContainer)
+    todoData.length = 0
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify([]))
+    saveAndRender()
+})
 
 // render existing todos
 render()
