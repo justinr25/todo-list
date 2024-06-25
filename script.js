c7a758f94da540f9d140ff4a55bf2f0c954cdda7
Justin <justin06.rosales@gmail.com>
2024-06-25 23:08:17 +0800

    added functional due date property to tasks

--- script.js
+++ script.js
@@ -8,14 +8,16 @@ const dropdown = document.querySelector('[data-dropdown]')
 const dropdownBtn = document.querySelector('[data-dropdown-btn]')
 const dropdownContent = document.querySelector('[data-dropdown-content]')
 const optionBtns = document.querySelectorAll('[data-option-btn]')
+const dueDateCalendar = document.querySelector('[data-due-date-calendar]')
+const dueDateText = document.querySelector('[due-date-text]')
 
 // define globals
 const LOCAL_STORAGE_LIST_KEY = 'task.list'
 const priorityFlags = {
-    'No Priority': 'assets/no-priority-flag.png',
-    'Low Priority': 'assets/low-priority-flag.png',
-    'Medium Priority': 'assets/medium-priority-flag.png',
-    'High Priority': 'assets/high-priority-flag.png'
+    'No Priority': './assets/no-priority-flag.png',
+    'Low Priority': './assets/low-priority-flag.png',
+    'Medium Priority': './assets/medium-priority-flag.png',
+    'High Priority': './assets/high-priority-flag.png'
 }
 
 // define utility functions
@@ -40,6 +42,7 @@ const renderTodo = (todo) => {
     //     <label for="task-1">
     //         <span class="custom-checkbox"></span>
     //         <p>Task 1</p>
+    //         <p>Due Date</p>
     //     </label>
     // </div>
 
@@ -61,8 +64,13 @@ const renderTodo = (todo) => {
     const todoName = document.createElement('p')
     todoName.innerHTML = todo.name
 
+    const dueDate = document.createElement('p')
+    dueDate.innerHTML = todo.dueDate
+    dueDate.classList.add('due-date-text')
+
     todoLabel.appendChild(customCheckbox)
     todoLabel.appendChild(todoName)
+    todoLabel.appendChild(dueDate)
     todoDiv.appendChild(tempCheckbox)
     todoDiv.appendChild(todoLabel)
     todosContainer.appendChild(todoDiv)
@@ -91,32 +99,79 @@ const saveAndRender = () => {
 }
 
 // define function that creates a todo
-const createTodo = (name, priority) => {
+const createTodo = (name, priority, dueDate) => {
     return {
         id: Date.now().toString(),
         name: name,
+        complete: false,
         priority: priority,
-        complete: false
+        dueDate: dueDate
     }
 }
 
 // define function that adds todos to the todo list array
-const addTodo = (name, priority) => {
-    newTodo = createTodo(name, priority)
+const addTodo = (name, priority, dueDate) => {
+    newTodo = createTodo(name, priority, dueDate)
     todoData.push(newTodo)
 }
 
 // define function to toggle dropdown menu
 const toggleDropdown = () => {
     if (dropdown.hasAttribute('data-active')) {
-        console.log('delete 1')
         delete dropdown.dataset.active
-    }
-    else {
-        console.log('hi')
+    } else {
         dropdown.dataset.active = ''
     }
 }
+
+// define function that formats date
+const formatDate = (date) => {
+    if (date.indexOf('-') >= 0) {
+        // input date format to locale date string
+        const dateArr = date.split('-')
+        const year = dateArr[0]
+        const month = dateArr[1].replace(/^0+/, '')
+        const day = dateArr[2].replace(/^0+/, '')
+        const formattedDate = `${month}/${day}/${year}`
+        return formattedDate
+    } else {
+        // locale date string to required input date format
+        const dateArr = date.split('/')
+        const year = dateArr[2]
+        const month = dateArr[0].padStart(2, '0')
+        const day = dateArr[1].padStart(2, '0')
+        const formattedDate = `${year}-${month}-${day}`
+        return formattedDate
+    }
+}
+
+// define function that handles date being changed
+const dueDateHandler = (dueDateValue) => {
+    const today = new Date()
+    const todayFormattedDate = today.toLocaleDateString('en-US')
+    const tomorrow = new Date(today.getTime() + 24 * 3600 * 1000)
+    const tomorrowFormattedDate = tomorrow.toLocaleDateString('en-US')
+
+    if (dueDateValue === todayFormattedDate) {
+        return 'Today'
+    } else if (dueDateValue === tomorrowFormattedDate) {
+        return 'Tomorrow'
+    } else {
+        return dueDateValue
+    }
+}
+
+// handle due date value being changed
+dueDateCalendar.addEventListener('change', (e) => {
+    const dueDateValue = formatDate(dueDateCalendar.value)
+    dueDateText.innerHTML = dueDateHandler(dueDateValue)
+})
+
+// handle setting default due date to today on page load
+document.addEventListener('DOMContentLoaded', (e) => {
+    const dueDateValue = formatDate(new Date().toLocaleDateString('en-US'))
+    dueDateCalendar.value = formatDate(new Date().toLocaleDateString('en-US'))
+})
 
 // handle dropdown button being clicked
 dropdownBtn.addEventListener('click', (e) => {
@@ -139,6 +194,7 @@ optionBtns.forEach((optionBtn) => {
         // change dropdown button based on priority selected
         const priority = optionBtn.dataset.priority
         const priorityFlag = priorityFlags[priority]
+
         dropdownBtn.querySelector('img').setAttribute('src', priorityFlag)
         dropdownBtn.querySelector('img').setAttribute('alt', priority + ' Flag')
         dropdownBtn.querySelector('p').innerHTML = priority
@@ -151,10 +207,13 @@ optionBtns.forEach((optionBtn) => {
 // handle new task event
 newTodoForm.addEventListener('submit', e => {
     e.preventDefault()
-    newTodoName = newTodoInput.value
-    if (newTodoName == null || newTodoName == '') return
+
+    const newTodoName = newTodoInput.value
     const priority = dropdownBtn.dataset.priority
-    addTodo(newTodoName, priority)
+    const dueDate = dueDateHandler(formatDate(dueDateCalendar.value))
+
+    if (newTodoName == null || newTodoName == '') return
+    addTodo(newTodoName, priority, dueDate)
     newTodoInput.value = null
     saveAndRender()
 })
